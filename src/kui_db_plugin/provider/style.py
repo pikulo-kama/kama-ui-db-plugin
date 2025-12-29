05816532e056ffcca257810af6dd166c662b7a73
Artur Drabazha <jhorr2222@gmail.com>
2025-12-29 17:39:55 +0200

    Add dynamic resource provider

--- src/kui_db_plugin/provider/style.py
+++ src/kui_db_plugin/provider/style.py
@@ -1,5 +1,6 @@
 from kui.core.app import KamaApplication
 from kui.dto.style import KamaColor, KamaComposedColor, KamaFont
+from kui.dto.type import DynamicResource
 
 from kui_db_plugin.database import db
 
@@ -27,3 +28,15 @@ def load_fonts(application: KamaApplication):
         )
 
         application.add_font(color)
+
+
+def load_dynamic_resources(application: KamaApplication):
+
+    for resource_row in db.retrieve_table("setup_resource"):
+        resource = DynamicResource(
+            resource_name=resource_row.get("resource_name"),
+            resource_path=resource_row.get("resource_path"),
+            color_code=resource_row.get("color")
+        )
+
+        application.add_dynamic_resource(resource)
