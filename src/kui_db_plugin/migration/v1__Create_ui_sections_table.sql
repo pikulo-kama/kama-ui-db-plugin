c39b5b5be4857c399f188c1e632004db7e89c897
Artur Drabazha <jhorr2222@gmail.com>
2026-01-16 22:02:59 +0200

    Remove controller from ui_sections table

--- src/kui_db_plugin/migration/v1__Create_ui_sections_table.sql
+++ src/kui_db_plugin/migration/v1__Create_ui_sections_table.sql
@@ -3,6 +3,5 @@ CREATE TABLE IF NOT EXISTS ui_sections (
     section_id      VARCHAR PRIMARY KEY,
     section_label   VARCHAR NOT NULL,
     section_icon    VARCHAR,
-    controller      VARCHAR,
     order_id        INTEGER NOT NULL
 );
